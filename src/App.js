271c934f38443f1cc10d17a046485737265e3f6c
Dejan Cvetkovski <djncvetkovski@gmail.com>
2024-01-06 23:22:33 +0100

    set router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header";
+import {
+  HomePage,
+  CategoryProductPage,
+  ProductSinglePage,
+  CartPage,
+  SearchPage,
+} from "./pages/index";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductSinglePage />} />
+          <Route path="/category/:category" element={<CategoryProductPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/search/:searchTerm" element={<SearchPage />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
