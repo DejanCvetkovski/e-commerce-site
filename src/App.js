45c68578a71c1284e278db98b4cb6738993cdc2e
Dejan Cvetkovski <djncvetkovski@gmail.com>
2024-01-06 23:17:47 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.scss";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
