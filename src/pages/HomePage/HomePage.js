04eed8c4064111c11046c1e2ceac38cd05a4d482
Dejan Cvetkovski <djncvetkovski@gmail.com>
2024-01-16 00:15:43 +0100

    get categotries

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -7,12 +7,14 @@ import {
   getAllProducts,
   getAllProductsStatus,
 } from "../../store/productSlice";
+import { getAllCategories } from "../../store/categorySlice";
 import ProductList from "../../components/ProductList/ProductList";
 import { STATUS } from "../../utils/status";
 import Loader from "../../components/Loader/Loader";
 
 const HomePage = () => {
   const dispatch = useDispatch();
+  const categories = useSelector(getAllCategories);
 
   useEffect(() => {
     dispatch(fetchAsyncProducts(50));
@@ -34,6 +36,19 @@ const HomePage = () => {
     }
   }
 
+  const catProductsOne = products.filter(
+    (product) => product.category === categories[0]
+  );
+  const catProductsTwo = products.filter(
+    (product) => product.category === categories[1]
+  );
+  const catProductsThree = products.filter(
+    (product) => product.category === categories[2]
+  );
+  const catProductsFour = products.filter(
+    (product) => product.category === categories[3]
+  );
+
   return (
     <main>
       <div className="slider-wrapper">
@@ -51,6 +66,46 @@ const HomePage = () => {
               ) : (
                 <ProductList products={tempProducts} />
               )}
+            </div>
+            <div className="categories-item">
+              <div className="title-md">
+                <h3>{categories[0]}</h3>
+                {productStatus === STATUS.LOADING ? (
+                  <Loader />
+                ) : (
+                  <ProductList products={catProductsOne} />
+                )}
+              </div>
+            </div>
+            <div className="categories-item">
+              <div className="title-md">
+                <h3>{categories[1]}</h3>
+                {productStatus === STATUS.LOADING ? (
+                  <Loader />
+                ) : (
+                  <ProductList products={catProductsTwo} />
+                )}
+              </div>
+            </div>
+            <div className="categories-item">
+              <div className="title-md">
+                <h3>{categories[2]}</h3>
+                {productStatus === STATUS.LOADING ? (
+                  <Loader />
+                ) : (
+                  <ProductList products={catProductsThree} />
+                )}
+              </div>
+            </div>
+            <div className="categories-item">
+              <div className="title-md">
+                <h3>{categories[3]}</h3>
+                {productStatus === STATUS.LOADING ? (
+                  <Loader />
+                ) : (
+                  <ProductList products={catProductsFour} />
+                )}
+              </div>
             </div>
           </div>
         </div>
